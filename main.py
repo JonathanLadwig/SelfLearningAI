9912e978a71c307ed86471f8304491a9027ed506
Jonathan Ladwig <217036862@stu.ukzn.ac.za>
2022-10-03 14:27:49 +0200

    Testing

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from StacTacToe import StacTacToe
+import numpy as np # helps with the math
 
-game = StacTacToe(3)
+epoch = 100000
+firstMove = [0]*9
+secondMove = np.zeros((9,9))
+game = StacTacToe(epoch)
 inputArrays = game.getInputs()
 outputArrays = game.getOutputs()
-for i in range(3):
-    array2D = game.convertTo2DArray(inputArrays[i])
-    game.printBoard(array2D)
-    print("")
+for i in range(epoch):
+    inputs = inputArrays[i]
+    if outputArrays[i] == 1:
+        firstMove[inputs[0]] += 1
+        secondMove[inputs[0]][inputs[1]] -= 1
+    elif outputArrays[i] == -1:
+        firstMove[inputs[0]] -= 1
+        secondMove[inputs[0]][inputs[1]] += 1
 
+print("First Move Array: ", firstMove)
+print("Second Move Array: ")
+print(secondMove)
